448a4bc26452304d99ea25467dbcddde54c41bab
lasayoung <127091926+lasayoung@users.noreply.github.com>
2023-12-07 11:24:54 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,1163 @@
+#include "Widget.h"
+#include <QApplication>
+#include"Sense2020Dll.h"
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+
+  Widget w;
+  w.show();
+  return a.exec();
+
+}
+/**
+ *
+ *
+DLL_Get_Pixel_Count: 512
+DLL_Start_Constantly_Grab_Spectra: 3
+pdblPwr:
+3462
+3548
+3460
+3284
+3300
+3524
+3388
+3316
+3194
+3518
+3442
+3366
+3230
+3474
+3356
+3320
+3294
+3526
+3314
+3324
+3332
+3472
+3400
+3380
+3280
+3408
+3418
+3414
+3218
+3498
+3472
+3364
+3228
+3566
+3542
+3422
+3284
+3556
+3406
+3416
+3244
+3516
+3472
+3324
+3214
+3498
+3450
+3330
+3248
+3528
+3464
+3254
+3200
+3524
+3416
+3260
+3164
+3600
+3424
+3428
+3298
+3486
+3550
+3356
+3250
+3546
+3424
+3328
+3288
+3566
+3604
+3700
+4096
+6232
+16502
+41386
+26352
+8286
+4882
+3686
+3302
+3590
+3440
+3348
+3252
+3544
+3446
+3206
+3202
+3496
+3436
+3384
+3170
+3506
+3400
+3306
+3192
+3488
+3414
+3398
+3334
+3500
+3406
+3306
+3220
+3468
+3444
+3314
+3184
+3456
+3300
+3396
+3244
+3460
+3352
+3280
+3246
+3474
+3344
+3426
+3210
+3484
+3368
+3328
+3206
+3534
+3454
+3348
+3180
+3436
+3370
+3350
+3164
+3482
+3402
+3246
+3298
+3458
+3448
+3356
+3298
+3518
+3398
+3254
+3248
+3536
+3368
+3320
+3244
+3544
+3412
+3264
+3280
+3484
+3458
+3230
+3186
+3504
+3434
+3314
+3116
+3476
+3338
+3388
+3268
+3498
+3412
+3432
+3172
+3490
+3410
+3270
+3190
+3482
+3394
+3322
+3176
+3496
+3464
+3344
+3198
+3466
+3474
+3280
+3286
+3466
+3406
+3422
+3194
+3490
+3468
+3428
+3260
+3368
+3430
+3298
+3186
+3498
+3312
+3364
+3188
+3402
+3436
+3392
+3192
+3488
+3390
+3294
+3272
+3510
+3398
+3342
+3184
+3420
+3408
+3386
+3196
+3452
+3428
+3258
+3236
+3516
+3388
+3344
+3254
+3518
+3356
+3322
+3180
+3496
+3394
+3282
+3250
+3460
+3380
+3288
+3256
+3416
+3410
+3322
+3164
+3516
+3374
+3326
+3184
+3586
+3464
+3288
+3234
+3426
+3368
+3326
+3210
+3520
+3404
+3336
+3228
+3472
+3366
+3338
+3204
+3432
+3422
+3320
+3170
+3480
+3414
+3326
+3200
+3482
+3422
+3284
+3210
+3474
+3374
+3346
+3208
+3512
+3366
+3438
+3264
+3428
+3458
+3368
+3242
+3546
+3408
+3244
+3188
+3530
+3350
+3298
+3178
+3430
+3374
+3262
+3236
+3550
+3392
+3376
+3290
+3440
+3358
+3304
+3238
+3462
+3508
+3370
+3218
+3468
+3408
+3428
+3276
+3552
+3342
+3278
+3210
+3554
+3304
+3358
+3280
+3522
+3318
+3326
+3184
+3520
+3372
+3314
+3206
+3502
+3378
+3350
+3104
+3560
+3370
+3274
+3184
+3416
+3316
+3338
+3264
+3502
+3388
+3272
+3198
+3564
+3392
+3336
+3156
+3448
+3404
+3368
+3238
+3550
+3444
+3320
+3206
+3528
+3306
+3320
+3240
+3500
+3418
+3264
+3252
+3474
+3342
+3286
+3182
+3592
+3394
+3378
+3322
+3510
+3388
+3316
+3222
+3496
+3378
+3338
+3156
+3452
+3354
+3356
+3204
+3502
+3330
+3234
+3228
+3472
+3450
+3296
+3270
+3470
+3384
+3286
+3228
+3492
+3362
+3326
+3256
+3562
+3342
+3304
+3182
+3450
+3378
+3270
+3152
+3464
+3344
+3352
+3238
+3418
+3432
+3322
+3206
+3562
+3384
+3398
+3202
+3488
+3410
+3262
+3210
+3502
+3370
+3310
+3264
+3470
+3400
+3392
+3244
+3406
+3392
+3224
+3254
+3502
+3366
+3388
+3236
+3462
+3342
+3314
+3098
+3466
+3384
+3398
+3170
+3462
+3296
+3326
+3264
+3438
+3336
+3316
+3158
+3454
+3394
+3392
+3188
+3436
+3388
+3374
+3248
+3492
+3406
+3214
+3280
+3454
+3378
+3340
+3176
+3438
+3332
+3356
+3180
+3402
+3422
+3352
+3276
+3584
+3344
+3300
+3254
+3520
+3414
+3318
+3126
+3476
+3390
+3346
+3202
+3466
+3412
+3354
+3154
+3462
+3416
+3300
+3236
+3522
+3288
+3292
+3186
+3446
+3344
+3278
+3260
+3462
+3468
+3316
+DLL_Get_Constantly_Grab_Peaks: 1
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+Ignored NaN, Inf, or -Inf value.
+pdblPeakWL:
+1.30444e-281
+3.50129e-313
+-0.0470142
+188.802
+1.81887e+174
+6.42879
+188.373
+2.6024e-303
+6.47635
+188.37
+2.65249e-312
+6.52391
+188.373
+2.65249e-312
+12.9997
+188.812
+0.06775
+13.0235
+188.814
+2.65249e-312
+13.0472
+188.817
+2.65249e-312
+19.523
+189.697
+0.134577
+19.4274
+190.4
+-0.873089
+19.4187
+189.69
+-0.873089
+25.8945
+189.61
+-0.0123456
+25.9032
+190.32
+0.0696333
+25.7823
+189.621
+0.0696333
+32.2581
+188.501
+-0.170326
+32.3552
+188.485
+2.65249e-312
+32.4531
+188.495
+2.65249e-312
+38.9289
+189.175
+0.104373
+38.8548
+189.881
+-0.676824
+38.8942
+189.172
+-0.676824
+45.37
+189.532
+0.055474
+45.3836
+189.532
+2.65249e-312
+45.3972
+189.534
+2.65249e-312
+51.873
+190.143
+0.0937281
+51.8065
+190.85
+-0.627725
+51.6342
+190.161
+-0.627725
+58.11
+188.542
+-0.242552
+58.2154
+188.516
+2.65249e-312
+58.3238
+188.522
+2.65249e-312
+64.7996
+188.902
+0.0585456
+71.2754
+189.282
+0.0585456
+71.2917
+189.283
+2.65249e-312
+71.308
+189.284
+2.65249e-312
+77.7838
+189.964
+0.104373
+77.7097
+190.67
+-0.687991
+77.5783
+189.972
+-0.687991
+84.0541
+188.753
+-0.185034
+84.1513
+188.735
+2.65249e-312
+84.2499
+188.744
+2.65249e-312
+90.7257
+189.333
+0.0906807
+90.6613
+190.041
+-0.587009
+90.681
+189.331
+-0.587009
+pdblPeakPwr:
+1.30444e-281
+3.54373e-312
+0
+0
+4.25285e-314
+4.25285e-314
+1.56724e-303
+4.70759e+238
+4.24399e-314
+1.37414e-281
+1
+0
+0
+1
+57
+29
+0
+0
+1
+4.94066e-324
+1
+0
+0
+1
+59
+358
+0
+0
+1
+4.94066e-324
+1
+0
+0
+1
+2
+329
+0
+0
+1
+4.70807e-316
+0
+5.89915e-312
+0
+5.89915e-312
+1
+4.70401e-316
+6.92279e-317
+0
+2.47639e-281
+1.29058e-281
+1.50143e+238
+3.64127e+238
+1.22594e+238
+2.4766e-281
+1
+0
+0
+0
+1
+0
+1
+0
+0
+1.89373e-281
+4.68226e-316
+nan
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+2.16894e-314
+1.90092e-281
+4.69002e-316
+0
+0
+1.90117e-281
+1
+0
+0
+0
+1
+0
+1
+0
+0
+4.68037e-316
+0
+nan
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+8.14846e-312
+1.26481e-321
+1
+2.16916e-314
+4.81714e-321
+8.15122e-309
+2.39407e-303
+0
+0
+1
+9.40707e-315
+0
+4.24399e-313
+0
+0
+0
+0
+0.25
+0.25
+1.86694e+238
+4.26667e+237
+ **/
